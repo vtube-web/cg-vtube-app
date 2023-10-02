312bfd5de1bbfed8341ea149889abafd742d02b8
hainguyen031 <hainguyendinh031@gmail.com>
2023-10-02 10:58:19 +0700

    create liked video screen + scss + mockAPI, update file format

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import videoReducer from "../features/video/videoSlice";
 import videoHistoryReducer from "../features/video/videoWatchedSlice";
 import isModalSearchReducer from "../features/studio/studioSlice";
+import videoLikedReducer from "../features/video/videoLikedSlice";
 
 export const store = configureStore({
   reducer: {
     video: videoReducer,
     videoHistory: videoHistoryReducer,
     isModalSreach: isModalSearchReducer,
-
+    videoLiked: videoLikedReducer,
   },
 });
--- src/screens/likedVideosScreen/LikedVideosScreen.jsx
+++ src/screens/likedVideosScreen/LikedVideosScreen.jsx
@@ -1,58 +1,76 @@
+import React from "react";
 import style from "../../assets/scss/Components/Watching/_likedVideos.module.scss";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  AiOutlineDownload,
-  AiOutlineMore,
-  AiFillCaretRight,
-} from "react-icons/ai";
-import { IoShuffleSharp } from "react-icons/io5";
-export default function LikedVideosScreen() {
+  getVideoLiked,
+  selectVideoLikedList,
+} from "../../features/video/videoLikedSlice";
+import { useEffect, useState } from "react";
+import Playlist from "../likedVideosScreen/Playlist";
+import ListLikedVideo from "./ListLikedVideo";
+import AllButton from "./AllButton";
+import VideosButton from "./VideosButton";
+import ShortsButton from "./ShortsButton";
+
+function LikedVideosScreen() {
+  const dispatch = useDispatch();
+  const videoList = useSelector(selectVideoLikedList);
+  const [activeButton, setActiveButton] = useState("All");
+
+  useEffect(() => {
+    if (videoList.length === 0) {
+      dispatch(getVideoLiked());
+      console.log("Getting videos...");
+    }
+  }, [videoList.length, activeButton]);
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  const sortedVideoList = [...videoList].sort(
+    (a, b) => b.dateLiked - a.dateLiked
+  );
+
+  const filterVideos = () => {
+    switch (activeButton) {
+      case "Videos":
+        return sortedVideoList.filter((video) => video.type === "video");
+      case "Shorts":
+        return sortedVideoList.filter((video) => video.type === "short");
+      default:
+        return sortedVideoList;
+    }
+  };
+
+  const filteredVideoList = filterVideos();
+
   return (
-    <div className={`${style.main} container`}>
-      <div className={`${style.playlist__liked} `}>
-        <div className={`${style.content} `}>
-          <div className={`${style.content__header} `}>
-            <div className={`${style.content__thumbnail} `}>
-              <div className={`${style.content__thumbnail__img} `}>
-                <img
-                  src="https://i.pinimg.com/564x/e5/55/1e/e5551e38ed6b63af9d5aed4090cb3e9d.jpg"
-                ></img>
-              </div>
-              <div className={`${style.content__thumbnail__icon} `}></div>
-            </div>
-          </div>
-          <div className={`${style.content__body} `}>
-            <div className={`${style.content__text__header} `}>
-              <p>Liked videos</p>
-            </div>
-            <div className={`${style.content__text__body} `}>
-              <div className={`${style.content__text__username} `}>
-                <p>Hai Nguyen</p>
-              </div>
-              <div className={`${style.content__text__data} `}>
-                <p>243 videos</p>
-              </div>
-              <div className={`${style.content__button} `}>
-                <button>
-                  <AiOutlineDownload size={24} />
-                </button>
-                <button>
-                  <AiOutlineMore size={24} />
-                </button>
-              </div>
-            </div>
-            <div className={`${style.playlist__button} `}>
-              <button>
-                <AiFillCaretRight size={30} />
-                <span>Play all</span>
-              </button>
-              <button>
-                <IoShuffleSharp size={30} />
-                <span>Shuffle</span>
-              </button>
-            </div>
-          </div>
+    <div className={`${style.main} main-container`}>
+      <Playlist videos={sortedVideoList} />
+      <div className={`${style.playlist__content} `}>
+        <div className={`${style.list__button__search} `}>
+          <AllButton
+            active={activeButton === "All"}
+            onClick={handleButtonClick}
+          />
+          <VideosButton
+            active={activeButton === "Videos"}
+            onClick={handleButtonClick}
+          />
+          <ShortsButton
+            active={activeButton === "Shorts"}
+            onClick={handleButtonClick}
+          />
+        </div>
+        <div className={`${style.list__videos} `}>
+          {filteredVideoList.map((video, index) => (
+            <ListLikedVideo key={index} index={index} {...video} />
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+export default LikedVideosScreen;
